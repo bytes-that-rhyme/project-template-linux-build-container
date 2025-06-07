47b8cf68e075c4a0fc08f9c3163b886a6dec3b19
Bytes That Rhyme <184341933+bytes-that-rhyme@users.noreply.github.com>
2025-06-06 22:25:51 -0700

    . b call scripts directly

--- .github/workflows/test-build.cmd
+++ .github/workflows/test-build.cmd
@@ -1,2 +1,3 @@
-echo build | call shell.cmd || exit /b 1
-call shell.cmd < .github\workflows\input.txt || exit /b 1
+call scripts\setup.cmd
+call build
+call sh < .github\workflows\input.txt || exit /b 1
