852a6ca2302fcac17d4bb807a1444260e265b2ad
Bytes That Rhyme <184341933+bytes-that-rhyme@users.noreply.github.com>
2025-06-06 22:36:01 -0700

    . b also test mounting and ensure src directory exists

--- .github/workflows/test-build.cmd
+++ .github/workflows/test-build.cmd
@@ -1,3 +1,5 @@
-call scripts\setup.cmd
-call build
+call scripts\setup.cmd || exit /b 1
+call build || exit /b 1
+mkdir src
+echo success>src\test.txt
 call sh < .github\workflows\input.txt || exit /b 1
